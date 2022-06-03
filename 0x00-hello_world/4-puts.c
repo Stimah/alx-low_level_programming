a5979c3aec963db88b4f58d3c46d4e6b4b606690
Stimah <sarangtenare@gmail.com>
2022-06-02 18:44:29 -0700

    difficult

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 /*  @Auth :Sarang Tenare  */
 #include <stdio.h>
 /*  main - starting point of our c programme     Return: 0 */
+
 /* main.c */
+
 '''int main(void)
+
 {
 			puts("\"Programming is like building a multilingual puzzle");
-						return (0);
+
+			return (0);
 }
 
