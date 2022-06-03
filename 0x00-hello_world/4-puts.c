a37b70d5d368b57cd826e27f32326dcdf4ac2ea0
Stimah <sarangtenare@gmail.com>
2022-06-02 18:29:04 -0700

    final4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
+/*
+ *  *  @Auth :Sarang Tenare
+ *   */
 #include <stdio.h>
-
-/*  main - Entry point */
+/**
+ *  *   main - starting point of our c programme
+ *   *     Return: 0
+ *    */
 int main(void)
 {
 			puts("\"Programming is like building a multilingual puzzle");
-					
-			return (0);
+						return (0);
 }
+
